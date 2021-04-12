c6ad9f4b243bea2fdd304c2a2e903f91dacd922d
josefemenias <pepecraft@icloud.com>
2021-04-12 12:41:33 +0200

    eso

---
+++ palmaurijs.js
@@ -0,0 +1,33 @@
+
+
+function palmauri(){
+    var jose
+    jose=prompt("Eres mauricon? (si o no)")
+
+if (jose=="si") {
+    alert("oleeee")
+
+}
+else{
+    alert("tu ere maricon a mi no me la cuelas") 
+}
+}
+
+function niveldemaricon(){
+
+    var comodegay
+    comodegay=prompt("del 1 ar die")
+    if (comodegay < 3) {
+        alert("no muy gay")
+        document.body.style.backgroundColor="blue"
+    }
+    else if (comodegay < 5) {
+        alert("algo gay")
+        document.body.style.backgroundColor="red"
+    }
+    else {
+        alert("bua muy muy gay")
+        document.body.style.backgroundColor="palevioletre"
+    }
+}
+
